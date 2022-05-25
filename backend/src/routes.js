3718359b81c7274e885192bb4ead803b86ca9e92
Mateus Cruz <mateuscruz22@gmail.com>
2022-05-25 12:50:36 -0300

    Config basic upload

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,12 @@
 const routes = require('express').Router()
+const multer = require('multer')
+const multerConfig = require('./config/multer')
 
 routes.get('/', (req, res) => {
+    return res.json({ text: 'Index' })
+})
+
+routes.post('/posts', multer(multerConfig).single('file'), (req, res) => {
     return res.json({ text: 'File Upload' })
 })
 
